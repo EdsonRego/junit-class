2161fc9eaa77069732abf82bfb61484a2853ab96
EdsonRego <edsonxrego@gmail.com>
2024-01-12 12:22:01 -0300

    JUnit - exception tests

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -33,4 +33,36 @@ public class AccountTests {
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
 
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		
+		double result = acc.fullWithdraw();
+		
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
+		
+	}
+	
+	@Test
+	public void shouldDecreaseBalanceWhenSufficientBalance() {
+		
+		Account acc = AccountFactory.createAccount(800.0);
+		
+		acc.withdraw(500.0);
+		
+		Assertions.assertEquals(300.0, acc.getBalance());
+	}
+	
+	@Test
+	public void withdrawShouldThrowExceptionWhenInsufficientBalance() {
+				
+		Assertions.assertThrows(IllegalArgumentException.class, ()->{
+			Account acc = AccountFactory.createAccount(800.0);
+			acc.withdraw(800.1);
+		});
+	}
 }
